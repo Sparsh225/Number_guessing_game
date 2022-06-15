243fb748619acf85a8eb81d9bbcc75816d85d2ba
Sparsh Rawat <92641998+Sparsh225@users.noreply.github.com>
2022-06-15 19:32:11 +0530

    adding clear() function

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from art import logo
 print(logo)
 import random
+from replit import clear
 def check(chance):
   state=True
-  while state==True:
+  while chance!=0:
     num=int(input("Make a guess:"))
     if(num>guess):
       chance=chance-1
@@ -18,6 +19,8 @@ def check(chance):
       state=False
       print(f"you have {chance} attempts remainig to guess the number")
       print("you guessed the right number , You won")
+  clear()    
+  print("You Loose, you dont have any attempts left ")
       
 print("Welcome to a number guessing game !")
 print("Im thinking of anumber between 1 and 100")
