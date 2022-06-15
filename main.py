902857ee45a58ed17805f9240ecbc4c3841c699b
Sparsh Rawat <92641998+Sparsh225@users.noreply.github.com>
2022-06-15 13:21:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from art import logo
+print(logo)
+import random
+def check(chance):
+  state=True
+  while state==True:
+    num=int(input("Make a guess:"))
+    if(num>guess):
+      chance=chance-1
+      print("To high !")
+      print(f"you have {chance} attempts remainig to guess the number")
+    elif(num<guess):
+      chance=chance-1
+      print("To low !")
+      print(f"you have {chance} attempts remainig to guess the number")
+    else:
+      chance=chance-1
+      state=False
+      print(f"you have {chance} attempts remainig to guess the number")
+      print("you guessed the right number , You won")
+      
+print("Welcome to a number guessing game !")
+print("Im thinking of anumber between 1 and 100")
+guess=random.randint(1,100)
+level=input("DO YOU WANT TO PLAY THE HARD LEVEL OR LOW LEVEL ? TYPE 'h' or 'l' ").lower()
+if(level=='h'):
+  chance=5
+  print("You have 5 attempts remainig to guess the number")
+  check(chance)
+else:
+  print("You have 10 attempts remaining to guess the number ")
+  chance=10
+  check(chance)
